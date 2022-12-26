cc08817b62a5f34f7e4e180e440c8f980fb0920d
pk <philipp.koller@cads-additive.com>
2022-12-26 12:28:41 +0100

    added basic rigidbodies

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    GameObject paperball;
+
+    private Transform origin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin = paperball.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
